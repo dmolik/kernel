b9c690b9a76b208afe143c1d3bbf1b5b22766b90
Dan Molik <dmolik@bnac.net>
2012-03-26 16:30:24 -0400

    The script is now converted to python,
    Further development will only be in python

--- make_kernel.py
+++ make_kernel.py
@@ -7,20 +7,27 @@ import multiprocessing
 import shutil
 import subprocess
 
-def ckpatcher():
-  urllib.urlretrieve ("http://ck.kolivas.org/patches/3.0/3.3/3.3-ck1/patch-3.3-ck1.bz2", "/usr/src/patch-3.3-ck1.bz2")
-  bz = bz2.BZ2File('/usr/src/patch-3.3-ck1.bz2', 'r')
-  with open('/usr/src/linux/patch-3.3-ck1', 'w') as f:
-    data = bz.read()
-    f.write(data)
-  bz.close()
-  os.chdir('/usr/src/linux')
-  subprocess.call(['patch', '-p1', 'patch-3.3-ck1'])
+def compileKernel():
+   os.chdir('/usr/src/linux')
+   cores = multiprocessing.cpu_count()
+   # Compile the kernel
+   subprocess.call(['make', '-j'+str(cores)])
+   subprocess.call(['make', 'modules_install'])
+   subprocess.call(['make', 'headers_install'])
+   
+def ckPatcher():
+   urllib.urlretrieve ("http://ck.kolivas.org/patches/3.0/3.3/3.3-ck1/patch-3.3-ck1.bz2", "/usr/src/patch-3.3-ck1.bz2")
+   bz = bz2.BZ2File('/usr/src/patch-3.3-ck1.bz2', 'r')
+   with open('/usr/src/linux/patch-3.3-ck1', 'w') as f:
+      data = bz.read()
+      f.write(data)
+   bz.close()
+   os.chdir('/usr/src/linux')
+   subprocess.call(['patch', '-p1', 'patch-3.3-ck1'])
 
 checkck = os.path.isfile('/usr/src/linux/patch-3.3-ck1')
-
 if not checkck:
-  ckpatcher() 
+  ckPatcher() 
 
 os.chdir('/usr/src/linux')
 
@@ -29,43 +36,21 @@ subprocess.call(['make', 'oldconfig'])
 shutil.copy2('/usr/src/linux/.config', '/usr/src/')
 
 cores = multiprocessing.cpu_count()
+
 subprocess.call(['make', '-j'+str(cores)])
 
+compileKernel()
+subprocess.call(['emerge', 'aufs3', 'nvidia-drivers', 'openafs-kernel', 'btrfs'])
+compileKernel()
 
-retcode = subprocess.Popen(["pwd","-P"], stdout=subprocess.PIPE).communicate()[0]
-version = retcode.strip("/usr/src/linux-")
+subprocess.call(['mount', '/boot'])
 
-#print retcode
-#retcode = subprocess.Popen(["make","oldconfig"], stdout=subprocess.PIPE).communicate()[0]
-#print retcode
+rentval = subprocess.Popen(["pwd","-P"], stdout=subprocess.PIPE).communicate()[0]
+version = rentval.strip("/usr/src/linux-")
+shutil.copy2('/usr/src/linux/x86/boot/bzImage', '/boot/kernel'+version+'ck')
 
-#cd /usr/src
-#patchck=`ls -1 patch*ck*` || ( wget http://ck.kolivas.org/patches/3.0/3.3/3.3-ck1/patch-3.3-ck1.bz2 && bzip2 -d patch-3.3-ck1.bz2 )
-#cd linux
-#if [[ -z `ls -1 $patchck` ]] ; then 
-#	cp ../$patchck .
-#	patch -p1 < $patchck
-#fi
-#cp ../.config .
-#vers=`pwd -P`
-#vers=${vers##'/'*'/'}
-#vers=${vers##'linux-'}
-#make oldconfig
-#cp .config ../
-#cores=`cat /proc/cpuinfo | sed '/cpu\ cores[\t\:\ ]*/!d' | sed '1q' | sed 's/cpu\ cores\t\:\ //'`
-#cpus=`cat /proc/cpuinfo | sed '/physical\ id[\t\:\ ]*/!d' | sed '$!d' | sed 's/physical\ id\t\:\ //'`
-#let "cpus=cpus+1"
-#let "total=cores*cpus"
-#make -j$total
-#make modules_install
-#make headers_install 
-#emerge aufs3 nvidia-drivers openafs-kernel
-#make -j$total
-#make modules_install
-#make headers_install 
-#mount /boot
-#cp arch/x86/boot/bzImage /boot/kernel-$vers-ck
-#cd /usr/src
-#boot-update
-#umount /boot
+subprocess.call(['boot-update'])
+
+subprocess.call(['umount', '/boot'])
+
 
